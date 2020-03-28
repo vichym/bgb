9fe031aae9d50239bfc7c5bd70f0da0d30190b52
Vichym <vmeas@macalester.edu>
2020-03-28 18:54:11 -0500

    Utilitze Layout.js page

--- src/Components/LoginPage.jsx
+++ src/Components/LoginPage.jsx
@@ -1,51 +1,29 @@
 /* credit : https://www.hawatel.com/blog/handle-window-resize-in-react/ */
 
-import React, { Component } from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Card, Button, CardTitle, Input } from 'reactstrap';
+import { Card, Button, CardTitle, Input, Row, Col } from 'reactstrap';
+import Layout from '../Layout/Layout'
+import { Link } from 'react-router-dom';
+import logo from '../Logo/BGB.png'
+const LoginPage = (props) => {
 
-class LoginPage extends Component {
+    return (
+        <Layout color='rgba(249, 193, 79, 1)'>
+            <Card body inverse color="danger" className=' col-8 col-sm-8 col-md-6 col-lg-4 col-xl-3 '>
+                <Row>
+                    <Col sm={12} className="justify-center">
+                        <img src={logo} width='50px' height='50px' alt='logo'/>
+                    </Col>
+                    <Col>
+                        <CardTitle className="d-flex justify-content-center bg-success" style={{ fontSize: '2rem' }}>Chat APP</CardTitle>
+                    </Col>
+                </Row>
+                <Input type="text" name="username" id="username" placeholder="Enter Your Name !" />
+                <Button color="secondary" tag={Link} to="/gamerooms">Join</Button>
+            </Card>
+        </Layout>
+    );
 
-    constructor() {
-        super();
-        this.state = {
-            height: '182px'
-        }
-    }
-    /**
-     * Calculate & Update state of new dimensions
-     */
-    updateDimensions() {
-        this.setState({ height: `${window.innerHeight}px` });
-    }
-
-    /**
-     * Add event listener
-     */
-    componentDidMount() {
-        this.updateDimensions();
-        window.addEventListener("resize", this.updateDimensions.bind(this));
-    }
-
-    /**
-     * Remove event listener
-     */
-    componentWillUnmount() {
-        window.removeEventListener("resize", this.updateDimensions.bind(this));
-    }
-
-    render() {
-        return (
-            <div class="card bg-warning " style={{ height: `${this.state.height}` }}>
-                <div class="card-body row justify-content-center align-items-center bg-">
-                    <Card body inverse color="danger"  className=' col-8 col-sm-8 col-md-6 col-lg-4 col-xl-3 '>
-                        <CardTitle className="d-flex justify-content-center" style={{fontSize:'2rem'}}>Chat APP</CardTitle>
-                        <Input type="text" name="username" id="username" placeholder="Enter Your Name !" />
-                        <Button color="secondary">Join</Button>
-                    </Card>
-                </div>
-            </div>
-        );
-    }
 }
 export default LoginPage
