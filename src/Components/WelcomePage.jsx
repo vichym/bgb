21ef78c72e5f503a2af065e0e39436aad73a9eba
Vichym <vmeas@macalester.edu>
2020-04-01 20:55:39 -0400

    modify structure

--- src/Components/WelcomePage.jsx
+++ src/Components/WelcomePage.jsx
@@ -4,26 +4,22 @@ import { Button, Row, Col } from 'reactstrap';
 import Context from '../Context';
 
 
-const  WelcomePageWithoutContext = (props) => {
+const WelcomePageWithoutContext = (props) => {
     console.log(props.context)
     return (
-        <Layout color1='rgba(125, 238, 242, 1)' color2 = 'rgba(249, 193, 79, 1)' classNames='col-10 col-sm-10 col-md-6 col-lg-4 col-xl-3 ' >
-                <Row>
-                    <Col>
-                        <h3 className="d-flex justify-content-center ">Welcome,</h3>
+        <Layout color1='rgba(125, 238, 242, 1)' color2='rgba(249, 193, 79, 1)' classNames='col-10 col-sm-10 col-md-6 col-lg-4 col-xl-3 d-flex' >
+            <Row className=" d-flex justify-content-center align-items-center mx-auto">
+                    <h3 className="justify-content-center d-inline-flex" >Welcome, </h3>
+                    <h3 className="d-inline-flex justify-content-center ">{props.context.state.username}!</h3>
+            </Row>
+            <Row >
+                <Col className=" ">
+                    <Button className="w-100" color="primary" to="/gamerooms">Join</Button>
                 </Col>
-                <Col>
-                        <h3 className="d-flex justify-content-center ">{props.context.state.username}!</h3>
-                    </Col>
-                </Row>
-                <Row >
-                    <Col className=" ">
-                        <Button className="w-100" color="primary" to="/gamerooms">Join</Button>
-                    </Col>
-                    <Col className="  ">
-                        <Button className="w-100" color="success" to="/gamerooms">Create</Button>
-                    </Col>
-                </Row>
+                <Col className="  ">
+                    <Button className="w-100" color="success" to="/gamerooms">Create</Button>
+                </Col>
+            </Row>
         </Layout>
     );
 }
