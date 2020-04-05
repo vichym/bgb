40f1f638d1e079a33f101fce539710e148ccc704
Vichym <vmeas@macalester.edu>
2020-04-05 14:11:57 -0400

    finished design

--- src/Components/DashboardPage/InputAmount.jsx
+++ src/Components/DashboardPage/InputAmount.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Button } from 'reactstrap';
+import { Button, Input, ButtonGroup } from 'reactstrap';
 
 function InputAmount(props) {
+    const increment = () => {
+        props.this.setState({ amount: props.amount + 1 })
+    }
+    const decrement = () => {
+        props.this.setState({ amount: props.amount - 1 })
+    }
     return (
-        <div>
-            <Button className="round">+</Button>
-        </div>
+        <ButtonGroup className="m-2 d-flex align-items-center">
+            <Button className="align-self-stretch m-1" onClick={decrement}>-</Button>
+            <Input type="number" name="amount" onChange={props.updateValue} value={props.amount} autoComplete placeholder="specify amount" />
+            <Button className="align-self-stretch m-1" onClick={increment}>+</Button>
+        </ButtonGroup>
     );
 }
-
 export default InputAmount;
