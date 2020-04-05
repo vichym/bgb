9d0e8296720399fb4cb019ccddef7886ea4bd6ca
Vichym <vmeas@macalester.edu>
2020-04-05 14:10:23 -0400

    link pages together with shared data

--- src/Components/LoginPage.jsx
+++ src/Components/LoginPage.jsx
@@ -2,32 +2,44 @@
 
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {  Button, Input, Row, Col } from 'reactstrap';
+import { Button, Input, Row, Col } from 'reactstrap';
 import Layout from '../Layout/Layout'
-import { Link } from 'react-router-dom';;
+import { Link } from 'react-router-dom'; import Context from '../Context';
 
+const LoginPageWithoutContext = (props) => {
 
-const LoginPage = (props) => {
+    const { app } = props.context
+    
+    const handleChange = (e) => {
+        /* Set update username by setState App.js */
+        app.setState({ username: e.target.value })
+    }
 
     return (
-        <Layout color1='rgba(249, 193, 79, 1)' color2 = 'rgba(61, 180, 255, 1)' classNames='col-10 col-sm-10 col-md-6 col-lg-4 col-xl-3 ' >
+        <Layout color1='rgba(249, 193, 79, 1)' color2='rgba(61, 180, 255, 1)' classNames='col-10 col-sm-10 col-md-6 col-lg-4 col-xl-3' >
             <Row>
                 <Col>
                     <h3 className="d-flex justify-content-center " style={{ fontSize: '2rem' }}>Chat APP</h3>
                 </Col>
             </Row>
             <Row >
-                <Col className="mx-auto col-9 col-sm-6 col-md-6 col-lg-8 col-xl-8 m-2">
-                    <Input type="text" name="username" id="username" placeholder="Enter Your Name !" />
-                </Col>
-            </Row>
-            <Row >
-                <Col className="mx-auto col-9 col-sm-6 col-md-6 col-lg-8 col-xl-8 ">
-                    <Button className="w-100" color="success" tag={Link} to="/gamerooms">Join</Button>
+                <Col className="mx-auto col-9 col-sm-6 col-md-6 col-lg-8 col-xl-8">
+                    <Input className="m-1" type="text" name="username" onChange={handleChange} id="username" placeholder="Enter Your Name !" />
+                    <Button className="w-100 m-1" color="success" disabled={!app.state.username} tag={Link} to='/welcome'>Join</Button>
                 </Col>
             </Row>
         </Layout>
     );
 
+}
+
+const LoginPage = () => {
+    return (
+        <Context.Consumer>
+            {(value) =>
+                <LoginPageWithoutContext context={value} />
+            }
+        </Context.Consumer>
+    )
 }
 export default LoginPage
--- src/Components/WelcomePage.jsx
+++ src/Components/WelcomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Layout from '../Layout/Layout';
 import { Button, Row, Col } from 'reactstrap';
 import Context from '../Context';
+import { Link } from 'react-router-dom';
 
 
 const WelcomePageWithoutContext = (props) => {
@@ -9,15 +10,15 @@ const WelcomePageWithoutContext = (props) => {
     return (
         <Layout color1='rgba(125, 238, 242, 1)' color2='rgba(249, 193, 79, 1)' classNames='col-10 col-sm-10 col-md-6 col-lg-4 col-xl-3 d-flex' >
             <Row className=" d-flex justify-content-center align-items-center mx-auto">
-                    <h3 className="justify-content-center d-inline-flex" >Welcome, </h3>
-                    <h3 className="d-inline-flex justify-content-center ">{props.context.state.username}!</h3>
+                    <h3 className="justify-content-center d-inline-flex" >Welcome,&nbsp;  </h3>
+                    <h3 className="d-inline-flex justify-content-center ">{props.context.app.state.username}!</h3>
             </Row>
             <Row >
                 <Col className=" ">
-                    <Button className="w-100" color="primary" to="/gamerooms">Join</Button>
+                    <Button className="w-100" color="primary" tag={Link} to='/joingame'>Join</Button>
                 </Col>
                 <Col className="  ">
-                    <Button className="w-100" color="success" to="/gamerooms">Create</Button>
+                    <Button className="w-100" color="success" tag={Link} to='/creategame'>Create</Button>
                 </Col>
             </Row>
         </Layout>
