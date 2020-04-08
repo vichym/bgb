e68e69ee77f7e6a4a31b60480d85ebc9b3d45c63
Vichym <vmeas@macalester.edu>
2020-04-08 00:07:03 -0400

    add socket calls and remove manual data

--- src/Components/DashboardPage/DashboardPage.jsx
+++ src/Components/DashboardPage/DashboardPage.jsx
@@ -1,49 +1,52 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import TopBar from './TopBar';
 import { Row, Col, Container } from 'reactstrap';
 import Context from '../../Context';
 import PlayerProfileComponent from './PlayerProfileComponent';
 import TransactionComponent from './TransactionComponent';
 import Log from './Log';
-
+import socket from '../../socket'
 class DashboardPageWithoutContect extends Component {
 
-    state = {
-        players: [
-            { name: "Jonh" },
-            { name: "Jackinson" },
-            { name: "Jerk" },
-            { name: "Jonh" },
-            { name: "Jackinson" },
-            { name: "Jerk" },
-            { name: "Jonh" },
-            { name: "Jackinson" },
-            { name: "Jerk" },
-        ],
-        asset: [
-            { name: "Gold", amount: 1000 },
-            { name: "Silver", amount: 600 },
-            { name: "Gold", amount: 1000 },
+    constructor(props) {
+        super(props)
+        this.state = {
+            players: [],
+            asset: [],
+            log: []
+        }
+    }
 
-        ]
+    componentDidMount() {
+        socket.on("init_game_data",
+            ({ message, data }) => {
+                this.setState({ asset: data.asset, players: data.players, log: data.log })
+                console.log("Socket.on init_game_data")
+            })
+        socket.on("message", (message) => {
+            this.setState({ log: [...this.state.log, message] })
+        })
+
     }
 
     render() {
-        const { username, gameCode, socket } = this.props.context.app.state
+        const { username, gameCode } = this.props.context.app.state
         return (
-            <div class="vh-100 bg-primary">
+            <div className="vh-100 bg-primary">
                 <TopBar gameCode={gameCode} />
                 <Container fluid>
                     <Row className=" justify-content-center d-flex">
-                        <Col className=" col-12 col-md-5 p-1 justify-content-center">
-                            <PlayerProfileComponent className="m-1" asset={this.state.asset} username={username} />
+                        <Col className=" col-12 col-md-5 p-1 align-items-stretch justify-content-center">
+                            <PlayerProfileComponent className="m-1 align-items-stretch" asset={this.state.asset} username={username} />
                         </Col>
                         <Col className=" col-12 col-md-7 p-1 justify-content-center">
                             <TransactionComponent className="m-1" players={this.state.players} asset={this.state.asset} />
-                            <Log className="m-1" />
+                            <Log className="m-1" log={this.state.log} />
                         </Col>
                     </Row>
+                    
                 </Container>
+
             </div>
         );
     }
